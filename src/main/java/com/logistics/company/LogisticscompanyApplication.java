aefea2ddefad4ef10f6e66ccb62fce96594940db
todorpopov <todorpopov02@gmail.com>
2026-01-02 14:10:44 +0200

    Init

---
+++ src/main/java/com/logistics/company/LogisticscompanyApplication.java
@@ -0,0 +1,13 @@
+package com.logistics.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticscompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticscompanyApplication.class, args);
+	}
+
+}
