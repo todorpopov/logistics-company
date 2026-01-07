17ce1d89cf74fc0eb476c628dc411f82f58ac82e
todorpopov <todorpopov02@gmail.com>
2026-01-07 23:14:17 +0200

    Add CORS config

--- src/main/java/com/logistics/company/configs/WebMvcConfig.java
+++ src/main/java/com/logistics/company/configs/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.logistics.company.configs;
 import com.logistics.company.interceptors.AuthInterceptor;
 import com.logistics.company.interceptors.LoggedInInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,5 +23,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor);
         registry.addInterceptor(loggedInInterceptor).addPathPatterns("/api/auth/log-in");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
